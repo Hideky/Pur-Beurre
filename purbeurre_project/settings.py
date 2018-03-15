64313e72c53b4148d76acad93c56a8a11bce9d4c
Hideky <soris-orami@live.fr>
2018-03-15 20:35:25 +0100

    Fix PSQL error 2

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '1y1450@!@dnzw4#jgk%c9hoi26g77++i-nh=k
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = True
+    DEBUG = False
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['pur-beurre.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pur-beurre.herokuapp.com']
 
 
 # Application definition
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
-#         'NAME': 'purbeurre', # le nom de notre base de données créée précédemment
-#         'USER': 'Hideki', # attention : remplacez par votre nom d'utilisateur !!
-#         'PASSWORD': 'test123',
-#         'HOST': '',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'purbeurre', # le nom de notre base de données créée précédemment
+        'USER': 'Hideki', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'test123',
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
